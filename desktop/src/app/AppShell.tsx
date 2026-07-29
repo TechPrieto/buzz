fa1b957bb152bbf3456dc541a429c95eb8018c37
npub1223z34hd7vtwc6qj4s7flsxkj644nlre2nthu7lrrmkumhu3xddsrx9r6w <52a228d6edf316ec6812ac3c9fc0d696ab59fc7954d77e7be31eedcddf91335b@buzz.block.builderlab.xyz>
2026-07-29 14:06:52 -0700

    feat(desktop): leave communities through relay
    Co-authored-by: Taylor Ho <taylorkmho@gmail.com>
    Signed-off-by: Taylor Ho <taylorkmho@gmail.com>

--- desktop/src/app/AppShell.tsx
+++ desktop/src/app/AppShell.tsx
@@ -754,7 +754,7 @@ export function AppShell() {
                         communitiesHook.activeCommunity?.id ?? null
                       }
                       onAddCommunity={addCommunityDialog.openDialog}
-                      onRemoveCommunity={(id) => void handleRemoveCommunity(id)}
+                      onRemoveCommunity={handleRemoveCommunity}
                       onReorderCommunities={communitiesHook.reorderCommunities}
                       onSwitchCommunity={handleSwitchCommunity}
                       onUpdateCommunity={communitiesHook.updateCommunity}
@@ -853,9 +853,7 @@ export function AppShell() {
                             onOpenAddCommunity={addCommunityDialog.openDialog}
                             onSendFeedback={() => setIsSendFeedbackOpen(true)}
                             onUpdateCommunity={communitiesHook.updateCommunity}
-                            onRemoveCommunity={(id) =>
-                              void handleRemoveCommunity(id)
-                            }
+                            onRemoveCommunity={handleRemoveCommunity}
                             onSwitchCommunity={handleSwitchCommunity}
                             onCreateAgent={() => requestOpenCreateAgent()}
                             selfPresenceStatus={presenceSession.currentStatus}
